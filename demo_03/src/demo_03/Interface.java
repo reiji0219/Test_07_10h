3018e822caf344309453065fb93bdbc2d7056731
reiji <reiji@192.168.2.102>
2024-07-10 20:32:01 +0900

    10h

---
+++ demo_03/src/demo_03/Interface.java
@@ -0,0 +1,6 @@
+package demo_03;
+
+public interface Interface {
+	public void execute();
+	
+}
--- demo_03/src/demo_03/Test_07_10h.java
+++ demo_03/src/demo_03/Test_07_10h.java
@@ -2,6 +2,26 @@ package demo_03;
 
 public class Test_07_10h 
 {
-
+	public static void main( String[] args )
+	{
+		Calc10h calc = new Calc10h( "Poly-morphism" );
+			calc.process( () -> {
+				System.out.println( "関心事を記述" );
+				
+				Interface save = Calc10h.create();
+					save.execute();
+					
+					
+			});
+			
+				
+			
+		
+		
+		
 	
+		
+			
 	}
+	
+}
