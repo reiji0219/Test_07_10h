f56fc7ff5980721dd5d666d3f729fcd4820eea2b
reiji <reiji@192.168.2.102>
2024-07-13 06:49:03 +0900

    Test_07_12_job

--- demo_03/src/demo_03/Test_07_12_job.java
+++ demo_03/src/demo_03/Test_07_12_job.java
@@ -1,5 +1,18 @@
 package demo_03;
 
-public class Test_07_12_job {
+public class Test_07_12_job
+{
+	public static void main( String[] args )
+	{
+	Calc12 calc = new Calc12( "Poly-morphism" );
+	calc.process( () -> {
+		System.out.println( "関心事を記述" );
+
+		Interface save = Calc12.create();
+			save.execute();
+
+	});
+
+	}
 
 }
